ad28b242981dc9f52a08a6ab938ed3c53a6a293c
AndZapata <andzapata514@gmail.com>
2020-05-13 12:20:56 -0500

    [Add] coins

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 1000f;
     public Rigidbody rb;
+    private int score = 0;
 
     public void FixedUpdate()
     {
@@ -15,5 +16,15 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(0, 0, (-1 * speed) * Time.deltaTime);
         if (Input.GetKey(KeyCode.D))
             rb.AddForce(speed * Time.deltaTime, 0, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score = score + 1;
+            Debug.Log("Score: " + score);
+            Destroy(other.gameObject);
+        }
     }
 }
